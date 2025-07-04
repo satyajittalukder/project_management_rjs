3ee63d66b323f70a60d2cd51fdd4bc1622af7225
satyajittalukder <dorhubs@gmail.com>
2025-07-04 14:21:42 +0530

    dynamic render of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { NewProject } from "./components/NewProject";
 import { NoProjectSelected } from "./components/NoProjectSelected";
 import { SideBar } from "./components/SideBar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    seletedProjectId: undefined,
+    projects: [],
+  })
+
+
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
+
     <main className="flex h-screen my-8 gap-8">
-      <SideBar />
+      <SideBar onStartAddProject={handleStartAddProject} />
       {/* <NewProject /> */}
-      <NoProjectSelected />
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import NoProjectImage from '../assets/no-projects.png'
 import { Button } from './Button'
 
-export const NoProjectSelected = () => {
+export const NoProjectSelected = ({ onStartAddProject }) => {
   return (
     <div className='flex flex-col items-center justify-center w-2/3 gap-4'>
       <img src={NoProjectImage} alt="no-project-img" className='h-16 w-16 object-contain mx-auto' />
       <h2 className='text-2xl font-bold text-stone-800'>No Project Selected</h2>
       <p className='text-stone-600'>Please select a project Or create a new project to get  started..</p>
-      <Button>+ Create New Project</Button>
+      <Button onClick={onStartAddProject}>+ Create New Project</Button>
     </div>
   )
 }
