adce19a686ce641031056b1b0e4ff7a2b23f71a5
satyajittalukder <dorhubs@gmail.com>
2025-07-04 11:38:24 +0530

    the props spread is added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Button = ({children}) => {
+export const Button = ({ children, ...props }) => {
   return (
-    <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-600 text-stone-50  hover:bg-stone-500">{children}</button>
+    <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-600 text-stone-50  hover:bg-stone-500" {...props}>{children}</button>
   )
 }
